7157603114e913939a3f12b14114e0814300527b
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-10 18:43:38 +0200

    Add files via upload

---
+++ 25.AssociativeArrays/1.CountRealNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _1.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                if (!counts.ContainsKey(numbers[i]))
+                {
+                    counts[currentNumber] = 1;
+                }
+                else
+                {
+                    counts[currentNumber]++;
+                }
+            }
+            foreach (KeyValuePair<int, int> kvp in counts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
