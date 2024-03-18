f59f8185977a5c149688d5e7e86fb440857db4b6
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-18 22:26:50 +0200

    Add files via upload

---
+++ 09.ExerciseDataType/04.SumOfChars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04.SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < input; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                sum += (int)symbol;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
