64b2bc39998213534992edc7ec66ccd4c1354429
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-18 22:26:33 +0200

    Add files via upload

---
+++ 09.ExerciseDataType/03.Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capacityOfPersons = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((decimal)persons / capacityOfPersons);
+            Console.WriteLine(courses);
+        }
+    }
+}
