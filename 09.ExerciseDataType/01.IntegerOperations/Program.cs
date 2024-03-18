38e8be912ae4c0a8164f76f8fc573cbb50de8092
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-18 22:25:54 +0200

    Add files via upload

---
+++ 09.ExerciseDataType/01.IntegerOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourt = int.Parse(Console.ReadLine());
+            int total = ((first + second) / third) * fourt;
+            Console.WriteLine(total);
+        }
+    }
+}
