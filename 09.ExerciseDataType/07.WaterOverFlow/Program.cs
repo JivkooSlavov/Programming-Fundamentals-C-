2b5c3078928829c0620e1f1da687c90dc326f903
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-20 00:01:55 +0200

    Add files via upload

---
+++ 09.ExerciseDataType/07.WaterOverFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.WaterOverFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfLines = int.Parse(Console.ReadLine());
+            int capacityOfTank = 255;
+            int allDosesWater = 0;
+
+            for (int i = 0; i < numbersOfLines; i++)
+            {
+                int doseOfWater = int.Parse(Console.ReadLine());
+                allDosesWater += doseOfWater;
+                if (allDosesWater>capacityOfTank)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    allDosesWater -= doseOfWater;
+                }
+            }
+            if (capacityOfTank>=allDosesWater)
+            {
+                Console.WriteLine(allDosesWater);
+            }
+        }
+    }
+}
