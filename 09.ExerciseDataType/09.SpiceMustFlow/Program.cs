6bb68c8d170c7fa814501e63ec66e2ae807d2483
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-20 00:03:01 +0200

    Add files via upload

---
+++ 09.ExerciseDataType/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int spice = 0;
+            int days = 0;
+
+            while (yield>=100)
+            {
+                days++;
+                spice += yield-26;
+                yield -= 10;
+            }
+            spice -= 26;
+            if (spice<0)
+            {
+                spice = 0;
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(spice);
+        }
+    }
+}
