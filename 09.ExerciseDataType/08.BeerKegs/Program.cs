0167dc712e02dd2b2c7bc489bc6acd2ec7f41efa
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-20 00:02:12 +0200

    Add files via upload

---
+++ 09.ExerciseDataType/08.BeerKegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string name = "";
+            float calculated = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                float volume = (float)Math.PI * (radius * radius) * height;
+                if (volume>calculated)
+                {
+                    calculated = volume;
+                    name = model;
+                }
+
+            }
+            Console.WriteLine(name);
+
+        }
+    }
+}
