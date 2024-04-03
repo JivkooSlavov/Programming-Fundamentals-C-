cb0552935f5e25fdad0be82cd4650822ddb2c8eb
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-03 20:14:11 +0300

    Add files via upload

---
+++ 11.ArraysLab/5.SumEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _5.SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumber = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                if (inputNumber[i] %2==0)
+                {
+                    sum += inputNumber[i];
+
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
