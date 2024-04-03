cc5b6c3fbc1a01b32741a083b827baf4f3d8b621
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-03 20:13:51 +0300

    Add files via upload

---
+++ 11.ArraysLab/3.RoundingNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace _3.RoundingNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+            }
+           
+        }
+    }
+}
