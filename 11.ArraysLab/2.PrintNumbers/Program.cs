d09389fb8699de795a77d934dc482943ad525a1f
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-03 20:13:35 +0300

    Add files via upload

---
+++ 11.ArraysLab/2.PrintNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _2.PrintNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[7]
+            {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+            int number = int.Parse(Console.ReadLine());
+            if (number >= 1 && number <= 7)
+            {
+                Console.WriteLine(days[number-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
