abe172c5a75410bd2da834bd20d1c4b26c04db66
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-08 22:03:11 +0300

    Add files via upload

---
+++ 12.ExerciseArrays/01.Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] wagons = new int[n];
+            int peopleSum = 0;
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                wagons[i] = people; 
+                peopleSum += people;
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+            Console.WriteLine(peopleSum);
+        }
+    }
+}
