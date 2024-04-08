9c095353dfa2afdc90cec0f8d4ea412571a178c8
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-08 22:05:26 +0300

    Add files via upload

---
+++ 12.ExerciseArrays/3.ZigZagArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3.ZigZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentInput = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2==0)
+                {
+                    firstArr[i] = currentInput[0];
+                    secondArr[i] = currentInput[1];
+                }
+                else
+                {
+                    secondArr[i] = currentInput[0];
+                    firstArr[i] = currentInput[1];
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstArr));
+            Console.WriteLine(string.Join(" ", secondArr));
+        }
+    }
+}
