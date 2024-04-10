e4f7573fcc7fcfada21f1b6a90284a8c80383069
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-10 18:49:03 +0300

    Add files via upload

---
+++ 12.ExerciseArrays/6.EqualSums/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _6.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int left = 0;
+                int right = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    left += numbers[j]; 
+                }
+                for (int k = i+1; k < numbers.Length; k++)
+                {
+                    right += numbers[k];
+                }
+                if (left==right)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
