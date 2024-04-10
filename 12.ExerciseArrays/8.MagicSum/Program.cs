ef2b5173dccbc77868aeb6368e27739c33bd1d49
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-10 18:50:01 +0300

    Add files via upload

---
+++ 12.ExerciseArrays/8.MagicSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _8.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int magicNumber = int.Parse(Console.ReadLine());
+            int[] newArr = new int[2]; 
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == magicNumber)
+                    {
+                            newArr[0] = numbers[i];
+                            newArr[1] = numbers[j];
+                            Console.WriteLine(string.Join(" ", newArr));
+                    }
+                }
+            }
+        }
+    }
+}
