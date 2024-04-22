30d4cae593999a233925498158957a0a02f21c7d
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-22 18:38:10 +0300

    Add files via upload

---
+++ 14.Methods/6.CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _6.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = CalculateArea(w, h);
+            Console.WriteLine(area);
+        }
+
+        static double CalculateArea(double w, double h)
+        {
+            return w * h;
+        }
+    }
+}
