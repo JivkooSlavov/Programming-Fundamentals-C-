d0cc818432dbdc280d543afb2a99ceca0bc5b847
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-22 18:37:46 +0300

    Add files via upload

---
+++ 14.Methods/5.Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _5.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string purch = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double price = 0;
+            price = kindOfOrder(purch, price);
+            double totalPrice = price * quantity;
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+
+        private static double kindOfOrder(string purch, double price)
+        {
+            if (purch == "coffee")
+            {
+                price = 1.50;
+            }
+            else if (purch == "water")
+            {
+                price = 1.00;
+            }
+            else if (purch == "coke")
+            {
+                price = 1.40;
+            }
+            else if (purch == "snacks")
+            {
+                price = 2.00;
+            }
+
+            return price;
+        }
+    }
+}
