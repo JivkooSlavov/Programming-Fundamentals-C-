7c8f6b6942d4e094105490e3248999fe58418ebd
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-30 23:09:58 +0300

    Add files via upload

---
+++ 14.Methods/8.MathPower/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _8.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numbers = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double totalResult = Math.Pow(numbers, power);
+            Console.WriteLine(totalResult);
+
+        }
+    }
+}
