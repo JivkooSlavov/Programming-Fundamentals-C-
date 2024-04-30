2385a7a0aeda9b2f4e5b71f78f0d37b7c041b10f
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-30 23:10:19 +0300

    Add files via upload

---
+++ 14.Methods/9.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+
+namespace _9.GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string type = Console.ReadLine();
+
+
+            if (type == "int")
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                 int greaterValue= GetMax(a, b);
+                Console.WriteLine(greaterValue);
+            }
+            else if (type == "char")
+            {
+                char a = char.Parse(Console.ReadLine());
+                char b = char.Parse(Console.ReadLine());
+                char greaterValue = GetMax(a, b);
+                Console.WriteLine(greaterValue);
+
+            }
+            else if (type == "string")
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                string greaterValue = GetMax(a, b);
+                Console.WriteLine(greaterValue);
+            }
+           
+
+        }
+        static int GetMax(int a, int b)
+        {
+            if (a>b)
+            {
+                return a;
+            }
+            return b;
+        }
+        static char GetMax(char a, char b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+        static string GetMax(string a, string b)
+        {
+            int result = a.CompareTo(b);
+
+            if (result>0)
+            {
+                return a;
+            }
+            return b;
+        }
+      
+    }
+}
