067486465db6334dfc4228c1f5d18400f53e19ea
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-04-30 23:09:43 +0300

    Add files via upload

---
+++ 14.Methods/7.RepeatString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _7.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repeat = int.Parse(Console.ReadLine());
+
+            string result = "";
+            result = FinishResult(input, repeat, result);
+            Console.WriteLine(result);
+        }
+
+        static string FinishResult(string input, int repeat, string result)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                result += input;
+            }
+
+            return result;
+        }
+    }
+}
