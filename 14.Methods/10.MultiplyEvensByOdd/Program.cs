8ad70779a328f30391ee4b909fa0f1a548e58ddf
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-02 17:41:49 +0300

    Add files via upload

---
+++ 14.Methods/10.MultiplyEvensByOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _10.MultiplyEvensByOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            numbers=Math.Abs(numbers);
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            while (numbers > 0) 
+            {
+                int currentNumber = numbers % 10;
+                if (currentNumber % 2 == 0)
+                {
+                    sumEven += currentNumber;
+                }
+                else if (currentNumber %2!=0)
+                {
+                    sumOdd+= currentNumber;
+                }
+                numbers /= 10;
+            }
+            Console.WriteLine(sumEven*sumOdd);
+        }
+    }
+}
