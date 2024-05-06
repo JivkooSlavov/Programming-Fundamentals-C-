df31abbd959e9fa281e1254d8f24f5fbe95d1d7a
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-06 17:53:10 +0300

    Add files via upload

---
+++ 14.Methods/11.MathOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            string function = Console.ReadLine();
+            int numberTwo = int.Parse(Console.ReadLine());
+            int result = AllResult(numberOne, function, numberTwo);
+            Console.WriteLine(result);
+
+        }
+
+        static int AllResult(int numberOne, string function, int numberTwo)
+        {
+            int result = 0;
+            if (function == "/")
+            {
+                result = numberOne / numberTwo;
+
+
+            }
+            else if (function == "*")
+            {
+                result = numberOne * numberTwo;
+         
+
+            }
+            else if (function == "+")
+            {
+                result = numberOne + numberTwo;
+      
+
+            }
+            else if (function == "-")
+            {
+                result = numberOne - numberTwo;
+   
+
+            }
+            return result;
+        }
+    }
+}
