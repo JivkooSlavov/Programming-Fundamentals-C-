c39c1ee9ab32b0a98aeb3314082fa41d0fef9111
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-06 17:55:38 +0300

    Add files via upload

---
+++ 15.MethodsExercise/1.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());
+            int minnumber = int.MaxValue;
+            minnumber = MethodMinNumber(one, two, three, minnumber);
+        }
+
+        static int MethodMinNumber(int one, int two, int three, int minnumber)
+        {
+            if (one <= minnumber)
+            {
+                minnumber = one;
+            }
+            if (two <= minnumber)
+            {
+                minnumber = two;
+            }
+            if (three <= minnumber)
+            {
+                minnumber = three;
+            }
+            Console.WriteLine(minnumber);
+            return minnumber;
+        }
+    }
+}
