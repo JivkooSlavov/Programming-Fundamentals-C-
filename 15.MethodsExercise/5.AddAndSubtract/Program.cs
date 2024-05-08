d8702b07c60fc947e758c74ac1cc61a5b80b1718
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-08 22:27:49 +0300

    Add files via upload

---
+++ 15.MethodsExercise/5.AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+
+        int result = Sum(first, second);
+        result = Substract(result, third);
+
+        Console.WriteLine(result);
+    }
+    static int Sum(int first, int second)
+    {
+        return first + second;
+    }
+
+    static int Substract(int result, int third)
+    {
+        return result - third;
+    }
+
+
+
+}
