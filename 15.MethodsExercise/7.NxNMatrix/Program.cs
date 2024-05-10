f3cb33bb9af52ade91f2b4e619019fc36c0467b3
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-10 14:10:00 +0300

    Add files via upload

---
+++ 15.MethodsExercise/7.NxNMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _15.MethodsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] arr = new int[number];
+
+            FillArray(number, arr);
+            PrintMatrix(arr);
+
+        }
+
+        static void FillArray(int number, int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = number;
+            }
+        }
+        static void PrintMatrix(int[] arr) 
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+        }
+    }
+}
