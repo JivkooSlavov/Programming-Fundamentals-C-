2969dc0bc03caabc99a6fd1a8b366e07a8a3fc88
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-10 14:10:28 +0300

    Add files via upload

---
+++ 15.MethodsExercise/8.FactorialDevison/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _8.FactorialDevison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            Factoriel(numberOne);
+            Console.WriteLine($"{(Factoriel(numberOne)/ Factoriel(second)):f2}");
+        }
+
+        static double Factoriel(int number)
+        {
+            double fac1 = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                fac1 *= i;
+            }
+            return fac1;
+        }
+    }
+}
