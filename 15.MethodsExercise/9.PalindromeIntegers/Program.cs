cf9338cbdff4ffcf5a40107cb4df0bb3ed36166d
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-11 21:55:36 +0300

    Add files via upload

---
+++ 15.MethodsExercise/9.PalindromeIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    { 
+        string intput=Console.ReadLine();
+        while (intput != "END")
+        {
+            bool isNumberPalindrome = IsNumberPalindrome(intput);
+            Console.WriteLine(isNumberPalindrome);
+            intput = Console.ReadLine();
+        }
+
+    }
+
+    static bool IsNumberPalindrome(string symbols)
+    {
+        int left = 0;
+        int right = symbols.Length - 1;
+        while (left < right)
+        {
+            if (symbols[left] != symbols[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
