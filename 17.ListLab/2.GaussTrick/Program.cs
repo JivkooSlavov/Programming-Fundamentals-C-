9b43a13a412f046dc6fbc35ae4e743fbb0ad4f21
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-12 15:33:10 +0300

    Add files via upload

---
+++ 17.ListLab/2.GaussTrick/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace _2.GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToList();
+
+            List < int> result = new List<int>();   
+
+            for (int i = 0; i < numbers.Count/2; i++)
+            {
+                int first = numbers[i];
+                int last = numbers[numbers.Count - i - 1];
+                result.Add(first+last);
+            }
+            if (numbers.Count%2!=0)
+            {
+                int middleNumber = numbers.Count / 2;
+                result.Add(numbers[middleNumber]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
