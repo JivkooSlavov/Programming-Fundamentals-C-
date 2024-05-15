4fc1623bacc038f8e6c9a68ca1eb9d72c1cccf5e
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-15 19:06:56 +0300

    Add files via upload

---
+++ 17.ListLab/3.MergingLists/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _3.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> second = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToList();
+            List<int> lastList = new List<int> ();
+            int interaction = Math.Max(first.Count, second.Count); 
+            for (int i = 0; i < interaction; i++)
+            {
+                if (i<first.Count)
+                {
+                    lastList.Add(first[i]);
+                }
+                if (i<second.Count)
+                {
+                    lastList.Add(second[i]);
+
+                }
+
+            }
+            Console.WriteLine(string.Join(" ",lastList));
+        }
+    }
+}
