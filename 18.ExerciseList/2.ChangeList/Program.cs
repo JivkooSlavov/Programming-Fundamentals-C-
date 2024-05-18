76f0bf2c4db46c34e25e20c417ef5798e76fd7fd
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-18 23:03:36 +0300

    Add files via upload

---
+++ 18.ExerciseList/2.ChangeList/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _2.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+            string comand = "";
+            while ((comand = Console.ReadLine()) != "end")
+            {
+                string[] function = comand.Split();
+                if (function[0] =="Delete")
+                {
+                    int digit = int.Parse(function[1]);
+                    numbers.RemoveAll(x => x ==digit);
+                }
+                else if (function[0] == "Insert")
+                {
+                    int digit = int.Parse(function[1]);
+                    int index = int.Parse(function[2]);
+                    numbers.Insert(index, digit);
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
