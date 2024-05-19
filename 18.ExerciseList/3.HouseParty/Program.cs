d44654265bd2e12cc48a944e76351d4f9d559ee8
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-19 17:27:52 +0300

    Add files via upload

---
+++ 18.ExerciseList/3.HouseParty/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _3.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalGuests = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+            for (int i = 0; i < totalGuests; i++)
+            {
+                string[] arguments = Console.ReadLine().Split();
+                string name = arguments[0];
+                if (arguments[2]=="going!")
+                {
+                    if (guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(name);
+                    }
+                }
+                else if (arguments[2]=="not")
+                {
+                    if (guests.Contains(name))
+                    {
+                        guests.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("\n", guests));
+        }
+    }
+}
