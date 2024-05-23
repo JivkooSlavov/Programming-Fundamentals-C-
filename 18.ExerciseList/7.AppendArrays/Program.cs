609920187b96e010c272896720a4af9bfb040384
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-23 17:36:08 +0300

    Add files via upload

---
+++ 18.ExerciseList/7.AppendArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] stringArrays = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries);
+            List<string> result = new List<string>();
+
+            for (int i = stringArrays.Length - 1; i >= 0; i--)
+            {
+                string sequence = stringArrays[i];
+                string[] array = sequence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                result.AddRange(array);
+            }
+
+
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
