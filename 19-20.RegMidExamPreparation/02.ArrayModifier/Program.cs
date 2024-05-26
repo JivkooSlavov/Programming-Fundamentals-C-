8dc0e9c38193bf9e8ff452fe3c27a4032c4877f0
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-26 18:44:10 +0300

    Add files via upload

---
+++ 19-20.RegMidExamPreparation/02.ArrayModifier/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _2.ArrayModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+           
+            string command = "";
+            while ((command=Console.ReadLine())!="end")
+            {
+                string[] array = command.Split();
+                string function = array[0];
+                if (function== "swap")
+                {
+                    int indexOne = int.Parse(array[1]);
+                    int indexTwo = int.Parse(array[2]);
+
+                    int save = numbers[indexOne];
+                    numbers[indexOne] = numbers[indexTwo];
+                    numbers[indexTwo] = save;
+
+                }
+                else if (function == "multiply")
+                {
+                    int indexOne = int.Parse(array[1]);
+                    int indexTwo = int.Parse(array[2]);
+
+                    numbers[indexOne] *= numbers[indexTwo];
+                }
+                else if (function == "decrease")
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        numbers[i] -= 1;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
