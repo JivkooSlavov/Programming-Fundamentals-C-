14f5d9afc84f13d3417af055896d8381edd07e43
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-28 17:16:08 +0300

    Add files via upload

---
+++ 19-20.RegMidExamPreparation/02.TheLift/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _2.TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int[] wagons = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacityOfWagon = 4;
+
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                int currentWagons = wagons[i];
+                if (currentWagons<4)
+                {
+                    int emptySpaces = capacityOfWagon - currentWagons;
+                    numberOfPeople -= emptySpaces;
+                    if (numberOfPeople<0)
+                    {
+                        wagons[i] = capacityOfWagon - Math.Abs(numberOfPeople);
+                        Console.WriteLine("The lift has empty spots!");
+                        Console.WriteLine(string.Join(" ", wagons));
+                        return;
+                    }
+                    wagons[i] = 4;
+                }
+            }
+            if (numberOfPeople != 0)
+            {
+                Console.WriteLine($"There isn't enough space! {numberOfPeople} people in a queue!");
+                Console.WriteLine(string.Join(" ", wagons));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", wagons));
+            }
+        }
+    }
+}
