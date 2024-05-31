816b0843cd006b409e343f465556a77dd306f9ee
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-05-31 19:03:41 +0300

    Add files via upload

---
+++ 19-20.RegMidExamPreparation/03.MovingTarget/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace TheLift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] operations = command.Split();
+                int index = int.Parse(operations[1]);
+                if (operations[0] == "Shoot")
+                {
+                    int power = int.Parse(operations[2]);
+                    if (index >= 0 && index < input.Count)
+                    {
+                        input[index] -= power;
+                        if (input[index] <= 0)
+                        {
+                            {
+                                input.RemoveAt(index);
+                            }
+                        }
+                    }
+                }
+                else if (operations[0] == "Add")
+                {
+                    int value = int.Parse(operations[2]);
+                    if (index >= 0 && index < input.Count)
+                    {
+                        input.Insert(index, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                    ///    0 4 
+                }  ///// { 1 , 3 , 4 ,5 ,6 }
+                else if (operations[0] == "Strike")
+                {
+                    int radius = int.Parse(operations[2]);
+                    if (index + radius <= input.Count - 1 && index - radius >= 0 && index >= 0 && index < input.Count)
+                    {
+                        for (int i = index + radius; i >= index - radius; i--)
+                        {
+                            input.RemoveAt(i);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("|", input));
+        }
+
+    }
+
+}
