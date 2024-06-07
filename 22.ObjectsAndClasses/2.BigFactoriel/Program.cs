86d04c2939d2c93372f6aaaac157946637cb50aa
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-06-07 09:43:05 +0300

    Add files via upload

---
+++ 22.ObjectsAndClasses/2.BigFactoriel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace _2.BigFactoriel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger big = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                big *= i;
+            }
+            Console.WriteLine(big);
+        }
+    }
+}
