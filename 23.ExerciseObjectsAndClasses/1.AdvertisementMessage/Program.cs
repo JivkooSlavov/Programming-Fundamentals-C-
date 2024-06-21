1be10c16ece555dcccb2b797e9d4bf396b23bfd5
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-06-21 13:56:43 +0300

    Add files via upload

---
+++ 23.ExerciseObjectsAndClasses/1.AdvertisementMessage/Program.cs
@@ -0,0 +1,61 @@
+﻿using _23.ExerciseObjectsAndClasses;
+
+namespace _23.ExerciseObjectsAndClasses
+{
+    class Advertisement
+    {
+        public string[] Phrases =
+         {
+            "Excellent product.",
+            "Such a great product.",
+            "I always use that product.",
+            "Best product of its category.",
+            "Exceptional product.",
+            "I can't live without this product."
+        };
+        public string[] Events =
+        {   "Now I feel good.",
+            "I have succeeded with this product.",
+            "Makes miracles.I am happy of the results!",
+            "I cannot believe but now I feel awesome.",
+            "Try it yourself, I am very satisfied.",
+            "I feel great!"
+        };
+        public string[] Authors = 
+        { 
+            "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"
+        };
+        public string[] Cities = 
+        {
+            "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" 
+        };
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Advertisement ad = new Advertisement();
+
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            int index = random.Next(ad.Phrases.Length);
+            string phrase = ad.Phrases[index];
+
+            index = random.Next(ad.Events.Length);
+            string events = ad.Events[index];
+
+            index = random.Next(ad.Authors.Length);
+            string authors = ad.Authors[index];
+
+            index = random.Next(ad.Cities.Length);
+            string cities = ad.Cities[index];
+
+            Console.WriteLine($"{phrase} {events} {authors} – {cities}");
+        }
+
+    }
+}
---
+++ 23.ExerciseObjectsAndClasses/2.Articles/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _02.Articles
+{
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Authors = author;
+
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Authors { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+        public void ChangeAuthors(string newAuthor)
+        {
+            Authors = newAuthor;
+        }
+        public void Rename(string newTitle)
+        {
+            Authors = newTitle;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Authors}";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articleArray = Console.ReadLine().Split(", ").ToArray();
+
+            Article article= new Article(articleArray[0], articleArray[1], articleArray[2]);
+
+            int commandCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < commandCount; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ");
+                if (command[0]=="Edit")
+                {
+                    article.Edit(command[1]);
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    article.ChangeAuthors(command[1]);
+                }
+                else if (command[0] == "Rename")
+                {
+                    article.Rename(command[1]);
+                }
+            }
+            Console.WriteLine(article);
+        }
+    }
+}
