0f9f320a71bcde7157fa52577527879063cf347c
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-06-25 18:38:52 +0300

    Add files via upload

---
+++ 23.ExerciseObjectsAndClasses/4.Student/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _4.Student
+{   class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+
+                string[] studentArray = Console.ReadLine().Split(" ").ToArray();
+
+                Student student = new Student();
+                student.FirstName = studentArray[0];
+                student.LastName = studentArray[1];
+                student.Grade = double.Parse(studentArray[2]);
+
+                students.Add(student);
+
+            }
+            students = students.OrderByDescending(x=> x.Grade).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, students));
+        }
+    }
+}
