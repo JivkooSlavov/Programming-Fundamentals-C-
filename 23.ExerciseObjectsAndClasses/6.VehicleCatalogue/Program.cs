930618b72634cf4ddcfc720f362d0eec616f8aad
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-06-25 18:39:58 +0300

    Add files via upload

---
+++ 23.ExerciseObjectsAndClasses/6.VehicleCatalogue/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _6.VehicleCatalogue
+{
+    class Vehicle
+    {
+        public string Type { get; set; }
+        public string Model {  get; set; }
+        public string Color {  get; set; }
+        public double HorsePower {  get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> catalogue = new List<Vehicle>();
+            string command = "";
+
+            while ((command=Console.ReadLine())!="End")   
+            {
+
+                string[] truckArray = command.Split();
+                string Type = truckArray[0];
+                string Model = truckArray[1];
+                string Color = truckArray[2];
+                double HorsePower = double.Parse(truckArray[3]);
+
+
+            }
+        }
+    }
+}
