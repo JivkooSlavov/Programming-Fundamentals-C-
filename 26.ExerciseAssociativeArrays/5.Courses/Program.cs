33d8d80541e03e159080e9121dbd8d51b8f1d188
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-07-11 19:16:12 +0300

    Add files via upload

---
+++ 26.ExerciseAssociativeArrays/5.Courses/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _05.Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Course> courses = new Dictionary<string, Course>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arguments = input.Split(" : ").ToArray();
+                string courseName = arguments[0];
+                string studentName = arguments[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    Course course = new Course(courseName);
+                    courses.Add(courseName, course);
+                }
+                courses[courseName].StudentNames.Add(studentName);
+            }
+            foreach (var kpv in courses)
+            {
+                Console.WriteLine(kpv.Value);
+            }
+        }
+    }
+    class Course
+    {
+        public string Name;
+        public List<string> StudentNames;
+
+        public Course(string name)
+        {
+            Name = name;
+            StudentNames = new List<string>();
+        }
+        public override string ToString()
+        {
+            string result = $"{Name}: {StudentNames.Count}\n";
+            for (int i = 0; i < StudentNames.Count; i++)
+            {
+                result += $"-- {StudentNames[i]}\n";
+            }
+            return result.Trim();
+        }
+
+    }
+}
