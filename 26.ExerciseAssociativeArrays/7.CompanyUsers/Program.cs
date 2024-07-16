8a7833530f6ed76fe895f1103bed9d476786bc46
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-07-16 17:09:47 +0300

    Add files via upload

---
+++ 26.ExerciseAssociativeArrays/7.CompanyUsers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace testova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> information = new Dictionary<string, List<string>>();
+            string command;
+            while ((command=Console.ReadLine())!= "End")
+            {
+                string[] input = command.Split(" -> ").ToArray();
+                string company = input[0];
+                string id = input[1];
+
+                if (!information.ContainsKey(company))
+                {
+                    information[company] = new List<string>();
+                }
+                if (!information[company].Contains(id))
+                {
+                    information[company].Add(id);
+                }
+            }
+            foreach (var kvp in information)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var asd in kvp.Value)
+                {
+                    Console.WriteLine($"-- {asd}");
+                }
+            }
+
+        }
+    }
+}
