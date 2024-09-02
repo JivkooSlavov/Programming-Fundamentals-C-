0245df07034c86e736d8c21c48fd586a91fe96b4
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-09-02 22:40:59 +0300

    Add files via upload

---
+++ 28.TextProcessing/1.ValidUsernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _1.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] users = Console.ReadLine().Split(", ");
+            foreach (string user in users)
+            {
+                if (user.Length>= 3 && user.Length<=16)
+                {
+                    if (isValidUsername(user))
+                    {
+                        Console.WriteLine(user);
+                    }
+                }
+            }
+
+        }
+        private static bool isValidUsername(string user)
+        {
+
+            return user.All(x => char.IsLetterOrDigit(x) || x == '-' || x== '_');
+        }
+    }
+}
