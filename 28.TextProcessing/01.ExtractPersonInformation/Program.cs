7a2f94f1821490d6af12da034e839f1a08790727
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-09-02 22:40:31 +0300

    Add files via upload

---
+++ 28.TextProcessing/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace _001.ExtractPersonInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                int startIndexName = input.IndexOf('@');
+                int endIndexName = input.IndexOf('|');
+                string name = input.Substring(startIndexName + 1, endIndexName - startIndexName - 1 );
+                int startIndexAge = input.IndexOf('#');
+                int endIndexAge = input.IndexOf('*');
+                string age = input.Substring(startIndexAge + 1, endIndexAge - startIndexAge - 1);
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
