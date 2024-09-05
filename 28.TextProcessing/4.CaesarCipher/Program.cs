0dfe600e6274f00d2df54b970bc655924005ed97
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-09-05 22:11:12 +0300

    Add files via upload

---
+++ 28.TextProcessing/4.CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace _4.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char original = input[i];
+                sb.Append((char)(original+3));
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
