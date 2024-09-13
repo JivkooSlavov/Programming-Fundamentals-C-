e6486edb7a0d3eef5d045699e840cd6088a4fd04
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-09-13 22:24:55 +0300

    Add files via upload

---
+++ 28.TextProcessing/9.BigInteger/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string bigNumberString = Console.ReadLine();
+        System.Numerics.BigInteger bigNumber = System.Numerics.BigInteger.Parse(bigNumberString);
+
+        int singleDigit = int.Parse(Console.ReadLine());
+
+        System.Numerics.BigInteger product = bigNumber * singleDigit;
+
+        Console.WriteLine(product);
+    }
+}
