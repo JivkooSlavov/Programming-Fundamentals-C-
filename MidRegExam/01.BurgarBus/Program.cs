9fe99e1c4052750d464a4b2f9cedcf6ca987e949
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-10-18 22:07:50 +0300

    Add files via upload

---
+++ MidRegExam/01.BurgarBus/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExamFirstTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCities = int.Parse(Console.ReadLine());
+            double totalProfitOfCities = 0;
+
+            for (int i = 1; i <= numberOfCities; i++)
+            {
+                string nameOfCity = (Console.ReadLine());
+                double income = double.Parse(Console.ReadLine());
+                double expense = double.Parse(Console.ReadLine());
+                double profit = income - expense;
+                if (i%3 == 0) 
+                {
+                    profit = income - (expense * 1.5);
+                }
+                if (i%5  ==0)
+                {
+                    profit = (income * 0.9) - expense;
+                }
+                totalProfitOfCities += profit;
+                Console.WriteLine($"In {nameOfCity} Burger Bus earned {profit:f2} leva.");
+            }
+            Console.WriteLine($"Burger Bus total profit: {totalProfitOfCities:f2} leva.");
+        }
+    }
+}
